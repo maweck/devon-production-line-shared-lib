f5c2e3a15fdb8272c33ee0abd2943abacfbbe038
Marvin Weck <marvin.weck@capgemini.com>
2018-10-18 15:24:17 +0200

    Fix iterator #3

--- vars/installJenkinsPlugin.groovy
+++ vars/installJenkinsPlugin.groovy
@@ -6,7 +6,7 @@ import java.util.logging.Logger
 // Installs the given list of Jenkins plugins if not installed already. 
 // Each entry needs to contain the plugin name and its version (e.g. git@2.0).
 @NonCPS
-def call(List pluginsToInstall) {
+def call(List<String> pluginsToInstall) {
   def logger = Logger.getLogger("")
   def newPluginInstalled = false
   def initialized = false
@@ -14,7 +14,7 @@ def call(List pluginsToInstall) {
   def pm = instance.getPluginManager()
   def uc = instance.getUpdateCenter()
 
-  for (int i=0; i < pluginsToInstall.size(); i++) {
+  for (int i = 0; i <= pluginsToInstall.size(); i++) {
     def String pluginName = pluginsToInstall[i]
 
     stage("Installation of '$pluginName' Jenkins Plugin.") {
