0cc89c11cad6d5c3e0fd1ac46d4f9c89394e7694
Marvin Weck <marvin.weck@capgemini.com>
2018-10-19 15:22:06 +0200

    Add a first set of constants

---
+++ vars/globalVariables.groovy
@@ -0,0 +1,12 @@
+#!/usr/bin/env groovy
+
+final String CONFLUENCE_BASE_URL = "http://confluence-core:8090/confluence"
+final String GERRIT_BASE_URL = "http://gerrit-core:8080/gerrit"
+final String GITLAB_BASE_URL = "http://gitlab-core:80/gitlab"
+final String GRAFANA_BASE_URL = "http://grafana-core:3000/grafana"
+final String GRAYLOG_BASE_URL = "http://graylog-core:9000/graylog"
+final String JENKINS_BASE_URL = "http://jenkins-core:8080/jenkins"
+final String JIRA_BASE_URL = "http://jira-core:8080/jira"
+final String NEXUS_BASR_URL = "	http://nexus3-core:8081/nexus3/"
+final String SCM_BASE_URL = "http://scm-core:8080/scm"
+final String SONARQUBE_BASe_URL = "http://sonarqube-core:9000/sonarqube"
