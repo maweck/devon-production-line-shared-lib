0ed581a594c9bccff745142d3fb90c6c30a23e4c
Tobias Näther <tobias.naether@capgemini.com>
2018-10-19 16:06:28 +0200

    encode only the descriptions

--- src/com/capgemini/productionline/configuration/GitLab.groovy
+++ src/com/capgemini/productionline/configuration/GitLab.groovy
@@ -19,35 +19,35 @@ class GitLab implements Serializable {
   }
 
   private int getGroupId (String groupname) {
-    def searchresult=this.context.httpRequest consoleLogResponseBody: true, customHeaders: [[maskValue: true, name: 'PRIVATE-TOKEN', value: accesstoken]], httpMode: 'GET', url: java.net.URLEncoder.encode('http://gitlab-core/gitlab/api/v4/groups?name='+groupname, "UTF-8")
+    def searchresult=this.context.httpRequest consoleLogResponseBody: true, customHeaders: [[maskValue: true, name: 'PRIVATE-TOKEN', value: accesstoken]], httpMode: 'GET', url: 'http://gitlab-core/gitlab/api/v4/groups?name='+groupname
     def jsonObject = this.context.readJSON text: searchresult.getContent()
     return jsonObject.id
   } 
 
   private int getProjectId (String groupname, String projectname) {
     def groupid=getGroupid(groupname)
-    def searchresult=this.context.httpRequest consoleLogResponseBody: true, customHeaders: [[maskValue: true, name: 'PRIVATE-TOKEN', value: accesstoken]], httpMode: 'GET', url: java.net.URLEncoder.encode('http://gitlab-core/gitlab/api/v4/groups/'+groupid+'/projects?name='+projectname, "UTF-8")
+    def searchresult=this.context.httpRequest consoleLogResponseBody: true, customHeaders: [[maskValue: true, name: 'PRIVATE-TOKEN', value: accesstoken]], httpMode: 'GET', url: 'http://gitlab-core/gitlab/api/v4/groups/'+groupid+'/projects?name='+projectname
     def jsonObject = this.context.readJSON text: searchresult.getContent()
     return jsonObject.id
   } 
 
   public createGroup(String groupname, String grouppath, String groupdesc, String grouptype) {
-    this.context.httpRequest consoleLogResponseBody: true, customHeaders: [[maskValue: true, name: 'PRIVATE-TOKEN', value: accesstoken]], httpMode: 'POST', url: java.net.URLEncoder.encode('http://gitlab-core/gitlab/api/v4/groups?name='+groupname+'&path='+grouppath+'&description='+groupdesc+'&visibility='+grouptype, "UTF-8")
+    this.context.httpRequest consoleLogResponseBody: true, customHeaders: [[maskValue: true, name: 'PRIVATE-TOKEN', value: accesstoken]], httpMode: 'POST', url: 'http://gitlab-core/gitlab/api/v4/groups?name='+groupname+'&path='+grouppath+'&description='+java.net.URLEncoder.encode(groupdesc, "UTF-8")+'&visibility='+grouptype
   }
 
   public createProject(String groupname, String projectname, String projectpath, String projectdescription, String branchname) {
     groupid=getGroupId(groupname)
     // create project in target group
-    this.context.httpRequest consoleLogResponseBody: true, customHeaders: [[maskValue: true, name: 'PRIVATE-TOKEN', value: accesstoken]], httpMode: 'POST', url: java.net.URLEncoder.encode('http://gitlab-core/gitlab/api/v4/projects?name='+projectname+'&path='+projectpath+'&namespace_id='+groupid+'&default_branch='+branchname+'description='+projectdescription, "UTF-8")
+    this.context.httpRequest consoleLogResponseBody: true, customHeaders: [[maskValue: true, name: 'PRIVATE-TOKEN', value: accesstoken]], httpMode: 'POST', url: 'http://gitlab-core/gitlab/api/v4/projects?name='+projectname+'&path='+projectpath+'&namespace_id='+groupid+'&default_branch='+branchname+'description='+java.net.URLEncoder.encode(projectdescription, "UTF-8")
   }
 
   public protectBranch(String group, String project, String branchname) {
     projectid=getProjectId(groupname)
-    this.context.httpRequest consoleLogResponseBody: true, customHeaders: [[maskValue: true, name: 'PRIVATE-TOKEN', value: accesstoken]], httpMode: 'PUT', url: java.net.URLEncoder.encode('http://gitlab-core/gitlab/api/v4/projects/'+projectid+'/repository/branches/'+branchname+'/protect', "UTF-8")
+    this.context.httpRequest consoleLogResponseBody: true, customHeaders: [[maskValue: true, name: 'PRIVATE-TOKEN', value: accesstoken]], httpMode: 'PUT', url: 'http://gitlab-core/gitlab/api/v4/projects/'+projectid+'/repository/branches/'+branchname+'/protect'
   }
 
   public unprotectBranch(String group, String project, String branchname) {
     projectid=getProjectId(groupname)
-    this.context.httpRequest consoleLogResponseBody: true, customHeaders: [[maskValue: true, name: 'PRIVATE-TOKEN', value: accesstoken]], httpMode: 'PUT', url: java.net.URLEncoder.encode('http://gitlab-core/gitlab/api/v4/projects/'+projectid+'/repository/branches/'+branchname+'/unprotect', "UTF-8")
+    this.context.httpRequest consoleLogResponseBody: true, customHeaders: [[maskValue: true, name: 'PRIVATE-TOKEN', value: accesstoken]], httpMode: 'PUT', url: 'http://gitlab-core/gitlab/api/v4/projects/'+projectid+'/repository/branches/'+branchname+'/unprotect'
   }
 }
