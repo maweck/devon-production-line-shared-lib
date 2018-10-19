325758f4e9571ac1f8766b15ca853b1d2196c60f
Marvin Weck <marvin.weck@capgemini.com>
2018-10-19 14:20:52 +0200

    Add POST Parameter

--- src/com/capgemini/productionline/templates/SonarQube.groovy
+++ src/com/capgemini/productionline/templates/SonarQube.groovy
@@ -1,16 +1,16 @@
 #!/usr/bin/groovy
 package com.capgemini.productionline.templates;
 
-import jenkins.plugins.http_request.*
-
 class SonarQube implements Serializable {
     def String username;
     def String sonarQubeBaseUrl;
+    def context;
 
     /**
     * Initiate a new 'SonarQube' instance by providing a username and the SonarQube base URL.
     */
-    SonarQube(String username, String sonarQubeBaseUrl)  {
+    SonarQube(context, String username, String sonarQubeBaseUrl)  {
+        this.context = context;
         this.username = username;
         this.sonarQubeBaseUrl = sonarQubeBaseUrl;
     }
@@ -19,7 +19,7 @@ class SonarQube implements Serializable {
     * This method is using the provided username to request an API Token.
     */
     def getAuthToken(String tokenName) {
-        response = httpRequest consoleLogResponseBody: true, customHeaders: [[maskValue: true, name: 'X-Forwarded-User', value: username], [maskValue: true, name: 'X-Forwarded-Groups', value: 'admins']], httpMode: 'POST', url: 'http://sonarqube-core:9000/sonarqube/api/user_tokens/generate?name=' + tokenName
+        response = this.context.httpRequest consoleLogResponseBody: true, customHeaders: [[maskValue: true, name: 'X-Forwarded-User', value: username], [maskValue: true, name: 'X-Forwarded-Groups', value: 'admins']], httpMode: 'POST', url: 'http://sonarqube-core:9000/sonarqube/api/user_tokens/generate?name=' + tokenName
         parsedJsonResponse = readJSON text: response.getContent()
 
         // Return a token 
