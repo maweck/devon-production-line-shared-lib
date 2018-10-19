aa8abb0e93a3a9b5681a606873cab616a865201e
Marvin Weck <marvin.weck@capgemini.com>
2018-10-19 09:06:21 +0200

    Initial commit of a SonarQube method

---
+++ src/com/capgemini/productionline/templates/SonarQube.groovy
@@ -0,0 +1,33 @@
+#!/usr/bin/groovy
+package com.capgemini.productionline.templates;
+
+class SonarQube implements Serializable {
+    String username;
+    String sonarQubeBaseUrl;
+
+    /**
+    * Initiate a new 'SonarQube' instance by providing a username and the SonarQube base URL.
+    */
+    SonarQube(String username, String sonarQubeBaseUrl)  {
+        this.username = username;
+        this.sonarQubeBaseUrl = sonarQubeBaseUrl;
+    }
+
+    /**
+    * This method is using X-Forwarded-Headers to request an API Token 
+    * from SonarQube for the user of this 'SonarQube' instance.
+    */
+    def getAuthToken() {
+        URL sonarQubeTokenGenerateUrl = new URL(sonarQubeBaseUrl + "/api/user_tokens/generate")
+
+        HttpURLConnection connection = (HttpURLConnection) sonarQubeTokenGenerateUrl.openConnection();
+        connection.addRequestProperty("X-Forwarded-User", username)
+        connection.addRequestProperty("X-Forwarded-Group", "sonarqube-admins")
+        connection.addRequestProperty("Accept", "application/json")
+        connection.with {
+            doOutput = true
+            requestMethod = 'GET'
+            println content.text
+        }
+    }
+}
