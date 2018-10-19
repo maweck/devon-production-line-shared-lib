dc28f546399a7285ec5cad7649b2f74b8c372492
Tobias Näther <tobias.naether@capgemini.com>
2018-10-19 16:50:52 +0200

    corrected id

--- src/com/capgemini/productionline/configuration/GitLab.groovy
+++ src/com/capgemini/productionline/configuration/GitLab.groovy
@@ -21,19 +21,18 @@ class GitLab implements Serializable {
   private String getGroupId (String groupname) {
     def searchresult=this.context.httpRequest consoleLogResponseBody: true, customHeaders: [[maskValue: true, name: 'PRIVATE-TOKEN', value: accesstoken]], httpMode: 'GET', url: 'http://gitlab-core/gitlab/api/v4/groups?name='+groupname
     def jsonObject = this.context.readJSON text: searchresult.getContent()
-    return jsonObject.id[0]
+    return String.valueOf(jsonObject.id).replace("[","").replace("]","")
   } 
 
   private String getProjectId (String groupname, String projectname) {
     def groupid=getGroupid(groupname)
     def searchresult=this.context.httpRequest consoleLogResponseBody: true, customHeaders: [[maskValue: true, name: 'PRIVATE-TOKEN', value: accesstoken]], httpMode: 'GET', url: 'http://gitlab-core/gitlab/api/v4/groups/'+groupid+'/projects?name='+projectname
     def jsonObject = this.context.readJSON text: searchresult.getContent()
-    return jsonObject.id[0]
+    return String.valueOf(jsonObject.id).replace("[","").replace("]","")
   } 
 
   public createGroup(String groupname, String grouppath, String groupdesc, String grouptype) {
-    this.context.httpRequest consoleLogResponseBody: true, customHeaders: [[maskValue: true, name: 'PRIVATE-TOKEN', value: accesstoken]], httpMode: 'POST', url: 'http://gitlab-core/gitlab/api/v4/groups?name='+groupname+'&path='+grouppath
-    //+'&description='+java.net.URLEncoder.encode(groupdesc, "UTF-8")+'&visibility='+grouptype
+    this.context.httpRequest consoleLogResponseBody: true, customHeaders: [[maskValue: true, name: 'PRIVATE-TOKEN', value: accesstoken]], httpMode: 'POST', url: 'http://gitlab-core/gitlab/api/v4/groups?name='+groupname+'&path='+grouppath+'&description='+java.net.URLEncoder.encode(groupdesc, "UTF-8")+'&visibility='+grouptype
   }
 
   public createProject(String groupname, String projectname, String projectpath, String projectdescription, String branchname) {
